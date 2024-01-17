1e7cde5498e05b9acff7282735086f66b76efaab
Diego Navarro <code@diegognt.dev>
2024-01-17 18:17:36 -0500

    Adds styles to the Typeahead component

--- src/components/typeahead/index.tsx
+++ src/components/typeahead/index.tsx
@@ -1,8 +1,13 @@
 function Typeahead() {
   return (
-    <search>
-      <form action="#" name="search">
-        <input name="typeahead" type="search" />
+    <search className="w-full">
+      <form className="w-full" action="#" name="search">
+        <input
+          className="h-14 md:h-20 transition ease-in-out dark:text-white text-2xl md:text-3xl block w-full rounded-md border-0 bg-white/30 focus:border-white focus:ring-4 focus:ring-white"
+          name="typeahead"
+          autoComplete="off"
+          type="search"
+        />
       </form>
     </search>
   );
