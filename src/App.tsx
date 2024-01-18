89408d15d3f3b7a77dcbda9ac0358d1768fb92e0
Diego Navarro <code@diegognt.dev>
2024-01-18 09:15:30 -0500

    Adds initial support for dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Typeahead from "./components/typeahead";
 function App() {
   return (
     <article className="w-full font-mono">
-      <h1 className="text-5xl leading-tight md:text-6xl mb-10 md:mb-16 text-center">A random Typeahead</h1>
+      <h1 className="text-5xl leading-tight md:text-6xl mb-10 md:mb-16 text-center text-white dark:text-gray-700">A random Typeahead</h1>
       <Typeahead />
     </article>
   );
