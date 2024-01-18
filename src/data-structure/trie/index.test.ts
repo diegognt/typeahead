1f4afcfb1659411734b033f10ddea0eefbbc69d6
Diego Navarro <code@diegognt.dev>
2024-01-18 11:12:56 -0500

    Adds the Node class to be used in a Trie

---
+++ src/data-structure/trie/index.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from "vitest";
+import { Trie, Node } from "./index";
+
+describe("The Node class", () => {
+  test("should be able to be instantiated", () => {
+    const node = new Node();
+
+    expect(node).toBeDefined();
+  });
+
+  test("should be instantiated with the 'value' property `undefined` by default", () => {
+    const node = new Node<Number>();
+
+    expect(node.value).toBe(undefined);
+  });
+
+  test("should be instantiated with the 'children' property empty by default", () => {
+    const node = new Node<Number>();
+
+    expect(node.children).toBeDefined();
+    expect(node.children).toBeTypeOf("object");
+    expect(node.children.size).toBe(0);
+  });
+
+  test("should assign a string value in the 'value' property ", () => {
+    const node = new Node<Number>();
+    node.value = 7;
+
+    expect(node.value).toBe(7);
+    expect(node.value).toBeTypeOf("number");
+  });
+});
+
