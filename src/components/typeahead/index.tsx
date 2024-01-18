4da0bc4cc2d3648431428e2615e4e5ad7cd1500f
Diego Navarro <code@diegognt.dev>
2024-01-18 09:15:30 -0500

    Adds initial support for dark theme

--- src/components/typeahead/index.tsx
+++ src/components/typeahead/index.tsx
@@ -3,7 +3,7 @@ function Typeahead() {
     <search className="w-full">
       <form className="w-full" action="#" name="search">
         <input
-          className="h-14 md:h-20 transition ease-in-out dark:text-white text-2xl md:text-3xl block w-full rounded-md border-0 bg-white/30 focus:border-white focus:ring-4 focus:ring-white"
+          className="h-14 md:h-20 transition ease-in-out dark:text-white text-2xl md:text-3xl block w-full rounded-md border-0 ring-0 bg-white/30 dark:bg-gray-700/30 focus:border-white focus:ring-4 focus:ring-white dark:focus:ring-gray-700"
           name="typeahead"
           autoComplete="off"
           type="search"
