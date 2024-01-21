092e724ec85d360679dcfe8d47b556554b8e8860
Diego Navarro <code@diegognt.dev>
2024-01-21 16:16:29 -0500

    Fixes the Trie data structure lint errors

--- src/data-structure/trie/index.test.ts
+++ src/data-structure/trie/index.test.ts
@@ -9,13 +9,13 @@ describe("The Node class", () => {
   });
 
   test("should be instantiated with the 'value' property `undefined` by default", () => {
-    const node = new Node<Number>();
+    const node = new Node<number>();
 
     expect(node.value).toBe(undefined);
   });
 
   test("should be instantiated with the 'children' property empty by default", () => {
-    const node = new Node<Number>();
+    const node = new Node<number>();
 
     expect(node.children).toBeDefined();
     expect(node.children).toBeTypeOf("object");
@@ -23,7 +23,7 @@ describe("The Node class", () => {
   });
 
   test("should assign a string value in the 'value' property ", () => {
-    const node = new Node<Number>();
+    const node = new Node<number>();
     node.value = 7;
 
     expect(node.value).toBe(7);
@@ -39,7 +39,7 @@ describe("The Trie class", () => {
   });
 
   test("should insert a word into the trie with a value using the 'insert' method", () => {
-    const trie = new Trie<Number>();
+    const trie = new Trie<number>();
     trie.insert("one", 1);
 
     expect(trie.root.children.size).toBe(1);
@@ -67,21 +67,21 @@ describe("The Trie class", () => {
   });
 
   test("should return the value of a word inserted into the trie using the 'valueOf' method", () => {
-    const trie = new Trie<Number>();
+    const trie = new Trie<number>();
     trie.insert("one", 1);
 
     expect(trie.valueOf("one")).toBe(1);
   });
 
   test("should return `undefined` if the word is not in the trie using the 'valueOf' method", () => {
-    const trie = new Trie<Number>();
+    const trie = new Trie<number>();
     trie.insert("one", 1);
 
     expect(trie.valueOf("two")).toBe(undefined);
   });
 
   test("should return an array of existing words when uses the 'possibleWords' method with an existing prefix", () => {
-    const trie = new Trie<Number>();
+    const trie = new Trie<number>();
     trie.insert("one", 1);
     trie.insert("two", 2);
     trie.insert("three", 3);
@@ -92,12 +92,11 @@ describe("The Trie class", () => {
   });
 
   test("should return an empty array when uses the 'possibleWords' method with a non-existing prefix", () => {
-    const trie = new Trie<Number>();
+    const trie = new Trie<number>();
     trie.insert("one", 1);
     trie.insert("two", 2);
     trie.insert("three", 3);
 
     expect(trie.possibleWords("f")).toEqual([]);
   });
-
 });
