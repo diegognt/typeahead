120656d2f0994d9d17a0bbeb06690377ac5f815a
Diego Navarro <code@diegognt.dev>
2024-01-22 15:52:48 -0500

    Implements the "PokemonProvider" in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Typeahead from "./components/typeahead";
+import { PokemonProvider } from "./contexts/PokemonContext";
 
 function App() {
   return (
-    <article className="w-full font-mono">
-      <h1 className="text-5xl leading-tight md:text-6xl mb-10 md:mb-16 text-center text-white dark:text-gray-700">A random Typeahead</h1>
-      <Typeahead />
-    </article>
+    <PokemonProvider>
+      <article className="w-full font-mono">
+        <h1 className="text-5xl leading-tight md:text-6xl mb-10 md:mb-16 text-center text-white dark:text-gray-700">
+          A random Typeahead
+        </h1>
+        <Typeahead />
+      </article>
+    </PokemonProvider>
   );
 }
 
--- src/contexts/PokemonContext/index.tsx
+++ src/contexts/PokemonContext/index.tsx
@@ -5,16 +5,22 @@ import usePokemonApi from "../../hooks/use-poke-api";
 
 const PokemonContext = createContext<Trie<Pokemon>>(new Trie());
 
-export function PokemonProvider({
-  children,
-}: {
+type PokemonProviderProps = {
   children: React.ReactNode;
-}): JSX.Element {
+};
+
+export function PokemonProvider(props: PokemonProviderProps): JSX.Element {
+  const { children } = props;
   const { data, isLoading, error } = usePokemonApi();
   const [pokemons, setPokemons] = useState<Trie<Pokemon>>(new Trie<Pokemon>());
 
   useEffect(() => {
-    if ((!data && isLoading) || error) return;
+    if (isLoading) return;
+
+    if (error) {
+      console.error("Error fetching data from PokeAPI");
+      return;
+    }
 
     const trie = new Trie<Pokemon>();
 
