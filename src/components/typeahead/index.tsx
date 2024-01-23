a4b8f90cbbadca7ac768e16524d3d0fd788130de
Diego Navarro <code@diegognt.dev>
2024-01-23 16:38:49 -0500

    Implements the "Input" component inside the "Typeahead" component

--- src/components/typeahead/index.tsx
+++ src/components/typeahead/index.tsx
@@ -1,15 +1,29 @@
+import {
+  ChangeEvent,
+  useDeferredValue,
+  useState,
+} from "react";
+import Input from "./Input";
+
 function Typeahead() {
+  const [query, setQuery] = useState<string>("");
+  const deferredQuery = useDeferredValue<string>(query);
+  const handleUserInput = (e: ChangeEvent<HTMLInputElement>) =>
+    setQuery(e.target.value);
+
   return (
-    <search role="search" className="w-full">
-      <form className="w-full" action="#" name="search">
-        <input
-          className="h-14 md:h-20 transition ease-in-out dark:text-white text-2xl md:text-3xl block w-full rounded-md border-0 ring-0 bg-white/30 dark:bg-gray-700/30 focus:border-white focus:ring-4 focus:ring-white dark:focus:ring-gray-700"
-          name="typeahead"
-          autoComplete="off"
-          type="search"
-        />
-      </form>
-    </search>
+    <>
+      <search role="search" className="w-full">
+        <form className="w-full" action="#" name="search">
+          <Input
+            name="search"
+            placeholder="Search"
+            onChange={handleUserInput}
+          />
+        </form>
+      </search>
+      <p className="text-white">{deferredQuery}</p>
+    </>
   );
 }
 
