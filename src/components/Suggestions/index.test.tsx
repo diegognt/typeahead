96ba6ee5e59be3ed38b3d7318f9e61a854e9174f
Diego Navarro <code@diegognt.dev>
2024-01-24 15:47:25 -0500

    Adds the 'Suggestion' component

--- src/components/Suggestions/index.test.tsx
+++ src/components/Suggestions/index.test.tsx
@@ -1,6 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import { describe, test, expect } from "vitest";
-import { SuggestionList } from ".";
+import { Suggestion, SuggestionList } from ".";
+
+// TODO: Make sure to test in the future the following:
+//   - The SuggestionList throws an error if the children are not Suggestion components
+//   - The SuggestionList component only renders the Suggestion component
 
 describe("The Suggestions component", () => {
   test("renders correctly", () => {
@@ -8,17 +12,29 @@ describe("The Suggestions component", () => {
       <li>Item 1</li>
     </SuggestionList>);
 
-    const suggestions = screen.getByRole("list");
+    const suggestions = screen.getAllByRole("list");
 
-    expect(suggestions).toBeInTheDocument();
+    expect(suggestions[0]).toBeInTheDocument();
   });
 
-  test("trhows an error if children are not 'li' elements", () => {
-    expect(() => {
-      render(<SuggestionList>
-        <div>Item 1</div>
-      </SuggestionList>);
-    }).toThrowError("SuggestionList only accepts 'li' elements as children");
+  test("have a spacing of 2 by default", () => {
+    render(<SuggestionList>
+      <li>Item 1</li>
+    </SuggestionList>);
+
+    const suggestions = screen.getAllByRole("list");
+
+    expect(suggestions[0]).toHaveClass("gap-2");
+  });
+
+  test("have a spacing of 4", () => {
+    render(<SuggestionList spacing={4}>
+      <li>Item 1</li>
+    </SuggestionList>);
+
+    const suggestions = screen.getAllByRole("list");
+
+    expect(suggestions[0]).toHaveClass("gap-4");
   });
 
   test("should have at least one 'li' children", () => {
@@ -34,4 +50,29 @@ describe("The Suggestions component", () => {
 });
 
 
-describe.skip("The Suggestion component", () => {});
+describe("The Suggestion component", () => {
+  test("renders correctly inside a 'SuggestionList' component", () => {
+    render(<SuggestionList>
+      <Suggestion>Item 1</Suggestion>
+    </SuggestionList>);
+
+    const suggestion = screen.getByRole("listitem");
+
+    expect(suggestion).toBeInTheDocument();
+    expect(suggestion).toHaveTextContent("Item 1");
+  });
+
+  test("renders correctly two Suggestion inside the 'SuggestionList' component", () => {
+    render(<SuggestionList>
+      <Suggestion>Item 1</Suggestion>
+      <Suggestion>Item 2</Suggestion>
+    </SuggestionList>);
+
+    const suggestions = screen.getAllByRole("listitem");
+
+    expect(suggestions.length).toBeGreaterThan(1);
+    expect(suggestions[0]).toHaveTextContent("Item 1");
+    expect(suggestions[1]).toHaveTextContent("Item 2");
+  });
+
+});
--- src/components/Suggestions/index.tsx
+++ src/components/Suggestions/index.tsx
@@ -1,15 +1,33 @@
-import { ReactElement } from "react";
+import { Children, ReactNode } from "react";
+
+// TODO: Make sure in the future of the following:
+//   - The SuggestionList throws an error if the children are not a Suggestion component/s
+//   - The SuggestionList component only renders the Suggestion component
+
 
 type SuggestionListProps = {
-  children: ReactElement<HTMLLIElement | HTMLLIElement[]>;
+  children: ReactNode;
+  spacing?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 };
 
-export function SuggestionList(props: SuggestionListProps): JSX.Element {
+type SuggestionProps = {
+  children: ReactNode;
+};
+
+export function SuggestionList(props: SuggestionListProps) {
+  const { children, spacing = 2 } = props;
+
+  return (
+    <ul className={`flex flex-col gap-${spacing}`}>
+      {Children.map(children, (child) => {
+        return child;
+      })}
+    </ul>
+  );
+}
+
+export function Suggestion(props: SuggestionProps) {
   const { children } = props;
 
-  if (children.type !== "li") {
-    throw new Error("SuggestionList only accepts 'li' elements as children");
-  }
-
-  return <ul>{children}</ul>;
+  return <li>{children}</li>;
 }
