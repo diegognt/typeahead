5ee45282a3cae436bae16411bdfe3528419fb50c
Diego Navarro <code@diegognt.dev>
2024-01-26 13:35:47 -0500

    Adds a Error boundry

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import Typeahead from "./components/typeahead";
-import { PokemonProvider } from "./contexts/PokemonContext";
+import Typeahead from "./components/Typeahead";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
-    <PokemonProvider>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
       <article className="w-full font-mono">
         <h1 className="text-5xl leading-tight md:text-6xl mb-10 md:mb-16 text-center text-white dark:text-gray-700">
           A random Typeahead
         </h1>
         <Typeahead />
       </article>
-    </PokemonProvider>
+    </ErrorBoundary>
   );
 }
 
