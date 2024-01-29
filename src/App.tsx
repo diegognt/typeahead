2aa5fd488cd2f937f8c68f10fc4280d6e97ee018
Diego Navarro <code@diegognt.dev>
2024-01-29 10:07:31 -0500

    Adds data to the Pokemon provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { ErrorBoundary } from "react-error-boundary";
 import Typeahead from "./components/Typeahead";
 import { PokemonProvider } from "./providers/PokemonProvider";
+import { useEffect } from "react";
+import usePokemonApi from "./hooks/use-poke-api";
+import { fromArrayOfPokemonsToTrie } from "./utilities/pokemons";
+
 
 function App() {
+  const {data, error} = usePokemonApi();
+
+  useEffect(() => {
+    if (error) return;
+  }, [error]);
   return (
     <ErrorBoundary fallback={<div>Something went wrong</div>}>
-      <PokemonProvider>
+      <PokemonProvider value={fromArrayOfPokemonsToTrie(data)}>
         <article className="w-full font-mono">
           <h1 className="text-5xl leading-tight md:text-6xl mb-10 md:mb-16 text-center text-white dark:text-gray-700">
             A random Typeahead
